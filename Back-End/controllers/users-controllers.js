4bcd1d10e9b6f944a8e9d3d3acfcc2613207211f
ranjeet <ranjeetgautam13032@gmail.com>
2021-08-18 15:44:12 +0530

    updated signup method

--- Back-End/controllers/users-controllers.js
+++ Back-End/controllers/users-controllers.js
@@ -51,7 +51,7 @@ const getUsers = async (req, res, next) => {
   res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
-const signup = catchAsync(async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -113,7 +113,52 @@ exports.logout = (req, res) => {
   res.status(200).json({ status: "success" });
 };
 
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exist.",
+        401
+      )
+    );
+  }
+
+  // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password! Please log in again.", 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
 exports.getUsers = getUsers;
-exports.signup = signup;
+// exports.signup = signup;
 // exports.login = login;
 // exports.getUsersNum = getUsersNum;
--- Back-End/routes/users-routes.js
+++ Back-End/routes/users-routes.js
@@ -7,17 +7,7 @@ const router = express.Router();
 
 // router.get("/", usersController.getUsers);
 
-router.post(
-  "/signup",
-  [
-    check("name").not().isEmpty(),
-    check("email")
-      .normalizeEmail() // Test@test.com => test@test.com
-      .isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
-  usersController.signup
-);
+router.post("/signup", usersController.signup);
 
 router.post("/login", usersController.login);
 router.get("/logout", usersController.logout);
