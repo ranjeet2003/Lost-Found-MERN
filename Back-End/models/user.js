11866b0e4157eb1a391fcd3434cda1f2b54e1d33
ranjeet <ranjeetgautam13032@gmail.com>
2021-08-18 15:20:26 +0530

    password encryption done on DB storing

--- Back-End/models/user.js
+++ Back-End/models/user.js
@@ -1,16 +1,61 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 6 },
-  //image: { type: String, required: true },
-  //docs: [{ type: mongoose.Types.ObjectId, required: true, ref: "Place" }],
-  mobile: { type: String, required: true, minlength: 10, maxlength: 10 },
-  // signed: { type: Boolean },
+  name: {
+    type: String,
+    required: [true, "Please tell us your name!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
 });
 
 userSchema.plugin(uniqueValidator);
