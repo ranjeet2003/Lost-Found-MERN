dfd0ced7445ba0bf2e7e26529af7cdde7261a1a7
ranjeet <ranjeetgautam13032@gmail.com>
2021-10-18 17:35:43 +0530

    email delivered using env variables

--- Back-End/util/email.js
+++ Back-End/util/email.js
@@ -1,13 +1,14 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config({ path: "./config.env" });
 
 const sendEmail = async (options) => {
   // 1) Create a transporter
   const transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
-      user: "c529cecbff843c",
-      pass: "1b0b30cf98f3f9",
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
